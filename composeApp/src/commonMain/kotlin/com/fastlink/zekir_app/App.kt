fbbdcc12fdd53b667f744883bfae4437354ca649
BilalSerwan <ensw02121@student.su.edu.krd>
2024-11-26 16:31:06 +0300

    add koin module for viewModels

--- composeApp/src/commonMain/kotlin/com/fastlink/zekir_app/App.kt
+++ composeApp/src/commonMain/kotlin/com/fastlink/zekir_app/App.kt
@@ -1,37 +1,47 @@
 package com.fastlink.zekir_app
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.navigation.compose.rememberNavController
+import com.fastlink.zekir_app.di.viewModelModule
+import com.fastlink.zekir_app.navigation.Navigation
+import com.fastlink.zekir_app.ui.theme.lightScheme
+import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import org.koin.compose.KoinApplication
+import zekirapp.composeapp.generated.resources.Bahij_Nassim_Regular
 import zekirapp.composeapp.generated.resources.Res
-import zekirapp.composeapp.generated.resources.compose_multiplatform
+
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+    MaterialTheme(
+        colorScheme = lightScheme,
+        typography = Typography(
+            titleMedium = TextStyle(
+                fontFamily = FontFamily(Font(resource = Res.font.Bahij_Nassim_Regular))
+            ),
+            titleLarge = TextStyle(
+                fontFamily = FontFamily(Font(resource = Res.font.Bahij_Nassim_Regular))
+            ),
+            bodyMedium = TextStyle(
+                fontFamily = FontFamily(Font(resource = Res.font.Bahij_Nassim_Regular))
+            )
+        ),
+    ) {
+        val navController = rememberNavController()
+        KoinApplication(
+            application = {
+                modules(
+                    viewModelModule
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        ) {
+            Navigation(navController = navController)
         }
     }
 }
